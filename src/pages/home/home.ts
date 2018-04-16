cf8d9746323cfd25d9110fd88a81b1f7f9820355
Rajesh <rajesh.thinktech@gmail.com>
2018-04-16 20:42:46 +0530

    first upload

---
+++ src/pages/home/home.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  public lat:any;
+  public lng:any;
+ 
+
+  constructor(public navCtrl: NavController, public geo:Geolocation) {
+
+  }
+
+  ionViewDidLoad()
+  {
+    this.geo.getCurrentPosition().then(pos=>{
+      this.lat = pos.coords.latitude;
+      this.lng = pos.coords.longitude;
+      
+    }).catch(err=>{
+      console.log(err);
+    })
+  }
+}
